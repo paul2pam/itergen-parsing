f41ed2b3826b2aef3c7eded251f3f154ca8a41ca
Paul <paul2pam@yahoo.com>
2026-03-11 20:42:13 -0500

    error handling

--- test_qwen.py
+++ test_qwen.py
@@ -8,20 +8,21 @@ with open("itergen-agent/prompt.txt", "r") as f:
     prompt = f.read()
 
 
-response = requests.post("http://localhost:11434/api/generate", json={
-    "model": "qwen2.5",
-    "prompt": prompt,
-    "stream": False,
-    "temperature": 0.0
-})
 
-msg = response.json()["response"]
-print(msg)
-
-parser = Lark(grammar)
-
-print("parse succeeded")
-
-tree = parser.parse(msg)
-
-print(tree.pretty())
+while(True):
+    try: 
+        response = requests.post("http://localhost:11434/api/generate", json={
+        "model": "qwen2.5",
+        "prompt": prompt,
+        "stream": False,
+        "temperature": 0.0
+        })
+        msg = response.json()["response"]
+        print(msg)
+        parser = Lark(grammar)
+        tree = parser.parse(msg)
+        print(tree.pretty())
+        break
+    except Exception as e:
+        print(f"{e} retrying...")
+        print("-" * 16)
